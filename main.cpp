2036d29cba553d27c5d9fb3d4c28b69ffabcd3a1
Wiktor Rzepka <rzepka.wiktor1@gmail.com>
2024-12-16 08:45:29 +0100

    Nowa metoda policz

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int policz(int a, int b) {
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W pierwszym branch'u 2 + 3 = " << std::endl;
+    std::cout << "W pierwszym branch'u 2 + 3 = " <<policz(2,3) <<std::endl;
     return 0;
 }
